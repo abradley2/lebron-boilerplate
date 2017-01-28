d9bb5c1c5122e7c0279e9fe616015f0f2ce92efe
Anthony Bradley <antbradley91@gmail.com>
2017-01-28 09:53:59 -0500

    base css

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 const css = require('sheetify')
 
-css('purecss/build/pure.css')
+css(require('purecss/build/pure.css'))
+css(require('./styles/base.css'))
 
 document.addEventListener('DOMContentLoaded', function () {
 	const choo = require('choo')
-	const app = choo()
 	const home = require('./pages/home')
+
+	const app = choo()
 
 	app.model(require('./models/home'))
 
